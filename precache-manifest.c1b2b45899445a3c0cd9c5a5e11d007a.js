68c1558aa9ed0c55aca463db4b1c0e9ba4c14915
Umair Mirza <umair_hp@hotmail.com>
2020-07-23 19:02:54 +0500

    Updates

---
+++ precache-manifest.c1b2b45899445a3c0cd9c5a5e11d007a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c6da8f815310dd00afa1a27aae0b0c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f8a566fcc02cef254c3c",
+    "url": "/monsters-rolodex/static/css/main.b6e45ed9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a566fcc02cef254c3c",
+    "url": "/monsters-rolodex/static/js/main.e531630d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
